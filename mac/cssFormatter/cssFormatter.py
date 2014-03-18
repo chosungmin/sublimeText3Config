7f2f5569b61b75d2312c003b82fbc3f738777626
chosungmin <chosungmin@naverui-iMac.local>
2014-03-18 13:34:02 +0900

    css formatter 추

---
+++ mac/cssFormatter/cssFormatter.py
@@ -0,0 +1,23 @@
+import sublime
+import sublime_plugin
+import urllib
+
+class CssFormatterCommand(sublime_plugin.TextCommand):
+	def run(self, edit):	
+		txt  = ''	
+		sels = self.view.sel()
+
+		for sel in sels:
+			txt += self.view.substr(sel)
+
+		cssFormatterURL = "http://dev.ui.naver.com/tool/n-met/cssFormatter/formatted2.php"
+		cssData = urllib.parse.urlencode({"org[]":txt, "from":"sublimetext"}).encode('UTF-8')
+		url = urllib.request.Request(cssFormatterURL, cssData)
+		url.add_header("Content-type", "application/x-www-form-urlencoded")
+		ResponseData = urllib.request.urlopen(url).read().decode("utf8", 'ignore')
+
+		for sel in sels:
+			self.view.erase(edit, sel)
+			
+		for region in self.view.sel():
+			self.view.insert(edit, region.end(), ResponseData);
